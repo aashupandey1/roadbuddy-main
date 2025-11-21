23b8b89468178c1b5ba08971d3d7e51fda93645a
aashupandey <00ashupandey11@gmail.com>
2025-11-21 22:14:05 +0530

    Added Railway MySQL connection

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ logging.basicConfig(level=logging.INFO)
 def get_db_connection():
     conn = mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
